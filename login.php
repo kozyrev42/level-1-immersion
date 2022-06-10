89f69356856c6c956e4f3c3df5a5a766d57e4deb
kozyrev42 <kozyrev054@gmail.com>
2022-06-10 11:07:46 +0700

    логирование реализовано

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php // обработчик формы авторизации
+session_start();
+require_once('functions.php');
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+function login($email, $password){
+    // проверка есть ли в базе емаил
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=my_php;charset=utf8", "root", "");
+    // проверка есть ли в базе емаил
+    // запрос, с меткой на которую передадим переменную
+    $query = "SELECT * FROM `users-dive` WHERE email=:email";
+    // нужно подготовить запрос, для безопасной отправки в бд
+    $statement = $pdo->prepare($query);
+    // в запросе, на метку передаём переменную и выполняем его
+    $statement -> execute(['email' => $email]);
+    // $user - может содержать запись из таблицы
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    // проверяем пароль
+    // хэшировынный пароль из базы
+    $hash_password=$user['password'];
+    // проверяем соответствует-ли введеный пароль, хешу пароля из бд
+    // результат записываем в $verify_password
+    $verify_password=password_verify($password, $hash_password);
+
+    // если $user содержит, и $verify_password = true, значит проверка проедена
+    if (!empty($user) && $verify_password) {
+        $status_login = true;
+    } else {
+        $status_login = false;
+    }
+
+    return $status_login;
+}
+
+$status_login = login($email, $password);
+
+if ($status_login) {
+    // если проверки пройдены, записывем в сессию массив (данные пользователя)
+    $_SESSION['user']=['email'=>$user['email'],'id'=>$user['id']];
+    redirect_to ('users.php');
+} else {
+    //  передаём ошибку
+    $_SESSION['error'] = true;
+    // возращаемся к форме
+    redirect_to ('page_login.php');
+}
--- page_login.php
+++ page_login.php
@@ -44,14 +44,14 @@
             <?php unset($_SESSION['success']); ?>
             <?php endif; ?>
 
-            <form action="">
+            <form action="login.php" method="post">
                 <div class="form-group">
                     <label class="form-label" for="username">Email</label>
-                    <input type="email" id="username" class="form-control" placeholder="Эл. адрес" value="">
+                    <input type="email" name="email" id="username" class="form-control" placeholder="Эл. адрес" value="">
                 </div>
                 <div class="form-group">
                     <label class="form-label" for="password">Пароль</label>
-                    <input type="password" id="password" class="form-control" placeholder="" >
+                    <input type="password" name="password" id="password" class="form-control" placeholder="" >
                 </div>
                 <div class="form-group text-left">
                     <div class="custom-control custom-checkbox">
@@ -61,6 +61,7 @@
                 </div>
                 <button type="submit" class="btn btn-default float-right">Войти</button>
             </form>
+
         </div>
         <div class="blankpage-footer text-center">
             Нет аккаунта? <a href="page_register.php"><strong>Зарегистрироваться</strong>
