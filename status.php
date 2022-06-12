64ad6f8c871e35538e9b9453f3667c20d7b6d1c5
kozyrev42 <kozyrev054@gmail.com>
2022-06-12 17:55:58 +0700

    реализован Статус

--- status.php
+++ status.php
@@ -1,8 +1,34 @@
 <?php
 session_start();
+require_once('functions.php');
+
+//проверка на авторизацию 
+if (is_not_logged_in()) {
+    // если не авторизован, то перенаправление на форму логирования
+    redirect_to('page_login.php');
+}
+
+// получаем данные редактируемого профиля
+$edit_id_user = $_GET['id'];
+$edit_user = get_user_by_id($edit_id_user);
+
+// действующий email, редактируемого юзера
+$current_email_edit_user = $edit_user['email'];
+
+// данные авторизованного
+$logged_id_user = $_SESSION['user']['id'];
+
+// если НЕ админ, проверить на автора, если не автор, перенаправляем
+if (is_not_admin()) {
+    if (is_not_author($logged_id_user, $edit_id_user)) {
+        set_flash_massage("danger", "Редактировать только свой профиль!");
+        redirect_to('users.php');
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
@@ -15,21 +41,22 @@ session_start();
     <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
     <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+        <a class="navbar-brand d-flex align-items-center fw-500" href="users.php"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="users.php">Главная <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" href="page_login.html">Войти</a>
-                </li>
+                </li> -->
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Выйти</a>
+                    <a class="nav-link" href="logout.php">Выйти</a>
                 </li>
             </ul>
         </div>
@@ -52,6 +79,9 @@ session_start();
                             <div class="panel-content">
                                 <div class="row">
                                     <div class="col-md-4">
+
+                                        <!-- скрытый input с id -->
+                                        <input type="hidden" name="id" id="simpleinput" class="form-control" value="<?php echo $edit_user['id']; ?>">
                                         <!-- status -->
                                         <div class="form-group">
                                             <label class="form-label" for="example-select">Выберите статус</label>
@@ -68,7 +98,7 @@ session_start();
                                 </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -78,34 +108,28 @@ session_start();
     <script src="js/vendors.bundle.js"></script>
     <script src="js/app.bundle.js"></script>
     <script>
+        $(document).ready(function() {
 
-        $(document).ready(function()
-        {
+            $('input[type=radio][name=contactview]').change(function() {
+                if (this.value == 'grid') {
+                    $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-g');
+                    $('#js-contacts .col-xl-12').removeClassPrefix('col-xl-').addClass('col-xl-4');
+                    $('#js-contacts .js-expand-btn').addClass('d-none');
+                    $('#js-contacts .card-body + .card-body').addClass('show');
 
-            $('input[type=radio][name=contactview]').change(function()
-                {
-                    if (this.value == 'grid')
-                    {
-                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-g');
-                        $('#js-contacts .col-xl-12').removeClassPrefix('col-xl-').addClass('col-xl-4');
-                        $('#js-contacts .js-expand-btn').addClass('d-none');
-                        $('#js-contacts .card-body + .card-body').addClass('show');
+                } else if (this.value == 'table') {
+                    $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-1');
+                    $('#js-contacts .col-xl-4').removeClassPrefix('col-xl-').addClass('col-xl-12');
+                    $('#js-contacts .js-expand-btn').removeClass('d-none');
+                    $('#js-contacts .card-body + .card-body').removeClass('show');
+                }
 
-                    }
-                    else if (this.value == 'table')
-                    {
-                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-1');
-                        $('#js-contacts .col-xl-4').removeClassPrefix('col-xl-').addClass('col-xl-12');
-                        $('#js-contacts .js-expand-btn').removeClass('d-none');
-                        $('#js-contacts .card-body + .card-body').removeClass('show');
-                    }
+            });
 
-                });
-
-                //initialize filter
-                initApp.listFilter($('#js-contacts'), $('#js-filter-contacts'));
+            //initialize filter
+            initApp.listFilter($('#js-contacts'), $('#js-filter-contacts'));
         });
-
     </script>
 </body>
+
 </html>
--- status_handler.php
+++ status_handler.php
@@ -1,14 +1,14 @@
 <?php
-var_dump($_POST['select']);
+require_once('functions.php');
+session_start();
 
+// получить данные
+$status = $_POST['status_select'];
+$id_user = $_POST['id'];
 
-
-
-
-
-
-
-
-
+// установить статус
+set_status($status, $id_user);
 
 // перенаправление на главную
+set_flash_massage("success", "Статус изменён!");
+redirect_to('users.php');
